d8422bcd94a99ee5830b43ce3af6fdb47102800b
HarisHad2 <57347089+HarisHad2@users.noreply.github.com>
2019-11-21 20:58:46 +0100

    Update tärningsim.py

--- tärningsim.py
+++ tärningsim.py
@@ -1,12 +1,36 @@
 import random
+import time
+
+def dicethrow():
+    return random.randrange(1, 7)
+
+list_of_throws = []
 
 dice = input("Vill du slå tärning?")
-num = random.randrange(1, 7)   
+time.sleep(1) 
 while dice == "ja":
-    num = random.randrange(1, 7)
-    print(num)
-    dice2 = input("Vill du slå tärning?")
-    
+    x = dicethrow()
+    print(x)
+    list_of_throws.append(x)
+    dice2 = input("Vill du slå igen?")
+    time.sleep(1)
+    if dice2 == "quit":
+        quit()
+    if dice2 == "mean":
+        print(float(mean_of_throws(list_of_throws)))
+
+
+def sum_of_throws(list_of_throws):
+    result = 0
+    for x in list_of_throws:
+        result = result + x
+    return result
+
+def mean_of_throws(list_of_throws):
+    result = 0 
+    for x in list_of_throws:
+        result = result + x
+    return result / len(list_of_throws)
  
 
 
